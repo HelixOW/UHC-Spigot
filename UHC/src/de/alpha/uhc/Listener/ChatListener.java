e2730fa40c4e744c79d7299a15f96f588e915e71
AlphaHelixDev <max.hertel@outlook.de>
2016-03-13 13:22:35 +0100

    Bugfixes

--- UHC/src/de/alpha/uhc/Listener/ChatListener.java
+++ UHC/src/de/alpha/uhc/Listener/ChatListener.java
@@ -17,24 +17,32 @@ public class ChatListener implements Listener {
 		
 		e.setCancelled(true);
 		
-		if(GState.isState(GState.INGAME) || GState.isState(GState.GRACE)) {
-			for(Player all : Core.getInGamePlayers()) {
-				if(e.getMessage().startsWith("#") && ATeam.hasSameTeam(e.getPlayer(), all)) {
-					all.sendMessage(Core.getPrefix() + "§7[" + ATeam.getTeamColor(ATeam.getPlayerTeam(e.getPlayer())) + ATeam.getPlayerTeam(e.getPlayer()) + "§7] " + e.getPlayer().getDisplayName() + " §7: " + e.getMessage().replaceFirst("#", ""));
-					return;
-				}
-				all.sendMessage(Core.getPrefix() + " " + e.getPlayer().getDisplayName() + "§7: " + e.getMessage());
-			}
-		} else {
-			Bukkit.broadcastMessage(Core.getPrefix() + " " + e.getPlayer().getDisplayName() + "§7: " + e.getMessage());
-		}
-		
 		if(Core.getSpecs().contains(e.getPlayer())) {
 			for(Player specs : Core.getSpecs()) {
 				specs.sendMessage("§7[§4X§7] §c" + e.getPlayer().getDisplayName() + "§7: " + e.getMessage());
 			}
+			return;
 		}
 		
+		if(GState.isState(GState.INGAME) || GState.isState(GState.GRACE) || GState.isState(GState.RESTART)) {
+			for(Player all : Core.getInGamePlayers()) {
+				if(!(e.getMessage().startsWith("#"))) {
+					all.sendMessage(Core.getPrefix() + " " + e.getPlayer().getDisplayName() + "§7: " + e.getMessage());
+				}
+			}
+		} else {
+			Bukkit.broadcastMessage(Core.getPrefix() + " " + e.getPlayer().getDisplayName() + "§7: " + e.getMessage());
+		}
+	}
+	
+	@EventHandler
+	public void onTeamChat(AsyncPlayerChatEvent e) {
+		e.setCancelled(true);
+		for(Player all : Core.getInGamePlayers()) {
+			if(e.getMessage().startsWith("#") && ATeam.hasSameTeam(e.getPlayer(), all)) {
+				all.sendMessage(Core.getPrefix() + "§7[" + ATeam.getTeamColor(ATeam.getPlayerTeam(e.getPlayer())) + ATeam.getPlayerTeam(e.getPlayer()) + "§7] " + e.getPlayer().getDisplayName() + " §7: " + e.getMessage().replaceFirst("#", ""));
+			}
+		}
 	}
 
 }
